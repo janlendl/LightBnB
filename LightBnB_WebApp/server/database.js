24db3c70d58669fa6a80814b32d8911d38c83432
Lendl Peralta <janlendl@gmail.com>
2021-10-14 20:55:09 +0000

    Create function to add property, have not tested it yet.

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -191,26 +191,6 @@ const getAllProperties = function(options, limit = 10) {
     .catch((err) => {
       console.log('Error: ', err.message);
     });
-
-
-
-
-
-
-
-
-
-
-
-  return pool
-    .query (allPropertiesQuery, [limit])
-    .then ((res) => {
-      console.log(res.rows);
-      return res.rows;
-    })
-    .catch ((err) => {
-    console.log(err.message);
-    });
 };
 
 exports.getAllProperties = getAllProperties;
@@ -222,9 +202,23 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const addPropertyQuery = `
+  INSERT INTO properties (title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, 
+  number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code) 
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING *`;
+
+  return pool
+    .query (addPropertyQuery, [title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, 
+      number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code])
+    .then ((res) => {
+      if (res.rows.length === 0) {
+        return null;
+      }
+      console.log(res.rows);
+      return res.rows;
+    })
+    .catch ((err) => {
+      console.log('Error: ', err.message);
+    });
 }
 exports.addProperty = addProperty;
