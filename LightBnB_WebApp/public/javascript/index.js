0d84891d01ea491f9779cf84c1976ab467645bb7
Lendl Peralta <janlendl@gmail.com>
2021-10-15 03:28:48 +0000

    create Jquery for reserve button to pass in the id

--- LightBnB_WebApp/public/javascript/index.js
+++ LightBnB_WebApp/public/javascript/index.js
@@ -2,5 +2,11 @@ $(() => {
   getAllListings().then(function( json ) {
     propertyListings.addProperties(json.properties);
     views_manager.show('listings');
+
+    $('.reserve-button').on('click', function() {
+      const idData = $(this).attr('id').substring(17);
+      views_manager.show('newReservation', idData);
+      console.log(idData);
+    });
   });
 });
