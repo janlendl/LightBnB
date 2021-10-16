c1558847574b2353cd50efe539501fe968ebd449
Lendl Peralta <janlendl@gmail.com>
2021-10-16 21:31:09 +0000

    Add switch statement for get update reservation

--- LightBnB_WebApp/public/javascript/views_manager.js
+++ LightBnB_WebApp/public/javascript/views_manager.js
@@ -1,6 +1,7 @@
 $(() => {
 
   const $main = $('#main-content');
+  let dataTag = '';
 
   window.views_manager = {};
 
@@ -11,29 +12,57 @@ $(() => {
     $logInForm.detach();
     $signUpForm.detach();
     $newReservationForm.detach();
+    $updateReservationForm.detach();
 
     switch (item) {
       case 'listings':
         $propertyListings.appendTo($main);
         break;
+
       case 'newProperty':
         $newPropertyForm.appendTo($main);
         break;
+
       case 'searchProperty':
         $searchPropertyForm.appendTo($main);
         break;
+
       case 'logIn':
         $logInForm.appendTo($main);
         break;
+
       case 'signUp':
         $signUpForm.appendTo($main);
         break;
+
       case 'newReservation':
-        const dataTag = `<h4>${data}</h4>`;
+        dataTag = `<h4>${data}</h4>`;
         $newReservationForm.appendTo($main);
         $('#datatag').empty();
         $(dataTag).appendTo('#datatag'); 
         break;
+
+      case 'updateReservation':
+        dataTag = `
+        <span id="datatag-reservation-id">${data.id}</span>
+        <span id="datatag-start-date">${data.start_date}</span>
+        <span id="datatag-end-date">${data.end_date}</span>
+        <span id="datatag-property-id">${data.property_id}</span>
+      `
+        const reservationDetails = `
+          <div id="reservation-details">
+            <h3>Reservation Details</h3>
+            <h4>Start date: ${moment(data.start_date).format("MMMM DD, YYYY")}</h4>
+            <h4>End date: ${moment(data.end_date).format("MMMM DD, YYYY")}</h4>
+          </div>
+        `
+        const errorMessage = data.error_message ? `<h4>${data.error_message}</h4>` : ``;
+        $(reservationDetails).appendTo($main);
+        $($updateReservationForm).appendTo($main);
+        $(dataTag).appendTo('#dataTag');
+        $(errorMessage).appendTo('#error-message');
+        break;
+
       case 'error': {
         const $error = $(`<p>${arguments[1]}</p>`);
         $error.appendTo('body');
@@ -41,7 +70,6 @@ $(() => {
           $error.remove();
           views_manager.show('listings');
         }, 2000);
-        
         break;
       }
     }
