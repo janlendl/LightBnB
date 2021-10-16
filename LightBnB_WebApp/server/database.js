f8e722dadeae6dff79ad95a676fbc73c4ce1721b
Lendl Peralta <janlendl@gmail.com>
2021-10-16 05:37:33 +0000

    Change getFulfilledReservations AND clause to  since the reservation should be already fulfilled

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -105,7 +105,7 @@ const getFulfilledReservations = function(guest_id, limit = 10) {
     JOIN properties ON reservations.property_id = properties.id
     JOIN property_reviews ON properties.id = property_reviews.property_id 
     WHERE reservations.guest_id = $1
-    AND reservations.start_date > now()::date
+    AND reservations.start_date < now()::date
     GROUP BY reservations.id, properties.id
     ORDER BY reservations.start_date
     LIMIT $2`;
