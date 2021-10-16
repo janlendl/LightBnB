6869cfee31a8a8c048d95500544e0e162b9ff806
Lendl Peralta <janlendl@gmail.com>
2021-10-16 03:18:29 +0000

    Move reserve button class as another parameter, this addressed the issue where you need to refresh the page in order for the reserve button to work

--- LightBnB_WebApp/public/javascript/index.js
+++ LightBnB_WebApp/public/javascript/index.js
@@ -3,7 +3,7 @@ $(() => {
     propertyListings.addProperties(json.properties);
     views_manager.show('listings');
 
-    $('.reserve-button').on('click', function() {
+    $('body').on('click', '.reserve-button', function() {
       const idData = $(this).attr('id').substring(17);
       views_manager.show('newReservation', idData);
       console.log(idData);
