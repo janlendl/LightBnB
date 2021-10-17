7a6329df437f0f3e78d029f0d59a28b8f38dc9ff
Lendl Peralta <janlendl@gmail.com>
2021-10-17 05:34:31 +0000

    Fix then promise. Added methods to call event listener on click update and delete

--- LightBnB_WebApp/public/javascript/components/property_listings.js
+++ LightBnB_WebApp/public/javascript/components/property_listings.js
@@ -23,22 +23,32 @@ $(() => {
     }
     // check for user login
     getMyDetails()
-    .then()
+    .then(() => {
       for (const propertyId in properties) {
-        if (isReservation) {
-          $('.update-button').on('click', function() {
-            const idData = $(this).attr('id').substring(16);
-            console.log(`update ${idData}`);          
-          })
-          $('.delete-button').on('click', function() {
-            const idData = $(this).attr('id').substring(16);
-            console.log(`delete ${idData}`);          
-          })
-        } 
         const property = properties[propertyId];
         const listing = propertyListing.createListing(property, isReservation);
         addListing(listing);
-    }
+      }
+      if (isReservation) {
+        $('.update-button').on('click', function() {
+          const idData = $(this).attr('id').substring(16);
+          getIndividualReservation(idData)
+            .then ((data) => {
+              views_manager.show('updateReservation', data);
+            });
+          console.log(`update ${idData}`);          
+        })
+
+        $('.delete-button').on('click', function() {
+          const idData = $(this).attr('id').substring(16);
+          console.log(`delete ${idData}`);          
+        })
+      } 
+    })
+    .catch((err) => {
+      console.log('Error: ', err);
+      return err.message;
+    });
   }
   window.propertyListings.addProperties = addProperties;
 
