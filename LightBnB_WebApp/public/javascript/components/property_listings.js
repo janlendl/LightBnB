1f9f5931e391720aaa1062697e7c31072fd40504
Lendl Peralta <janlendl@gmail.com>
2021-10-17 23:55:59 +0000

    Add this in order for the listings not to clear out the main screen

--- LightBnB_WebApp/public/javascript/components/property_listings.js
+++ LightBnB_WebApp/public/javascript/components/property_listings.js
@@ -45,6 +45,20 @@ $(() => {
           console.log(`delete ${idData}`); 
           $(this).closest('article').remove();   
         })
+
+        $('.add-review-button').on('click', function() {
+          const idData = $(this).attr('id').substring(11);
+          views_manager.show("newReview", idData);
+        })
+      } else {
+        $('.reserve-button').on('click', function() {
+          const idData = $(this).attr('id').substring(17);
+          views_manager.show('newReservation', idData);
+        })
+        $('.review_details').on('click', function() {
+          const idData = $(this).attr('id').substring(15);
+          views_manager.show('showReviews', idData);
+        })
       } 
     })
     .catch((err) => {
