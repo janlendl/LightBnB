bf1163a6edf3cc758327a8ba117fdd139da9def8
Lendl Peralta <janlendl@gmail.com>
2021-10-17 23:47:29 +0000

    Create a new post route for adding a new review

--- LightBnB_WebApp/server/apiRoutes.js
+++ LightBnB_WebApp/server/apiRoutes.js
@@ -92,15 +92,30 @@ module.exports = function(router, database) {
     const reservationId = req.params.reservation_id;
     database.deleteReservation(reservationId);
   });
-  
+
   router.get('/reviews/:propertyId', (req, res) => {
     const propertyId = req.params.propertyId
     database.getReviewsByProperty(propertyId)
     .then(reviews => {
       res.send(reviews);
     })
-  })
+    .catch((err) => {
+      console.log('Error: ', err);
+      res.send(err);
+    });
+  });
 
+  router.post('/reviews/:reservationId', (req, res) => {
+    const reservationId = req.params.reservationId;
+    database.addReview({...req.body})
+    .then(review => {
+      res.send(review);
+    })
+    .catch((err) => {
+      console.log('Error: ', err);
+      res.send(err);
+    });
+  });
 
   return router; 
 }
