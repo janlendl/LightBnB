f518fc6a59182801510bac638213f08585fd04e5
Lendl Peralta <janlendl@gmail.com>
2021-10-17 05:32:25 +0000

    Create update reservation AJAX call function

--- LightBnB_WebApp/public/javascript/network.js
+++ LightBnB_WebApp/public/javascript/network.js
@@ -74,3 +74,11 @@ const submitReservation = function(data) {
     data,
   });
 }
+
+const updateReservation = function(data) {
+  return $.ajax({
+    method: "POST",
+    url: `/api/reservations/${data.reservation_id}`,
+    data,
+  });
+}
