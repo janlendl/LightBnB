f4f87e12b644e15ed7be2f07332a07d761ed375a
Lendl Peralta <janlendl@gmail.com>
2021-10-18 21:22:35 +0000

    Update to cancel button

--- LightBnB_WebApp/public/javascript/components/new_property_form.js
+++ LightBnB_WebApp/public/javascript/components/new_property_form.js
@@ -81,7 +81,7 @@ $(() => {
 
         <div class="new-property-form__field-wrapper">
             <button>Create</button>
-            <a id="property-form__cancel" href="#">Cancel</a>
+            <button id="property-form__cancel" href="#">Cancel</button>
         </div>
         
     </form>
