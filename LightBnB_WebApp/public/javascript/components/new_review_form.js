0c976197553733222e37fc268611bd383cb51aa5
Lendl Peralta <janlendl@gmail.com>
2021-10-18 20:08:44 +0000

    Add function for submit button to call the submit query to insert the review and grab the individual reservation

--- LightBnB_WebApp/public/javascript/components/new_review_form.js
+++ LightBnB_WebApp/public/javascript/components/new_review_form.js
@@ -14,7 +14,33 @@ $(() => {
       <div id="datatag" class="hidden"></div>
       <button type="submit">Submit</button>
     </form>
-  `)
+  `);
+
+  $newReviewForm.on('submit', function(event) {
+    event.preventDefault();
+    const reviewBody = $('#new-review-body').val();
+    const reviewRating = $('#new-review-rating').val();
+    const reservationId = $('#datatag h4').text();
+    // clear our review fields
+    $('#new-review-rating').val("");
+    $("#new-review-body").val("");
+    if (reviewRating && reservationId) {
+      getIndividualReservation(reservationId)
+      .then(data => {
+        const dataObj = {...data, reservationId: data.id, message: reviewBody, rating: reviewRating};
+        submitReview(dataObj)
+        .then(result => {
+          views_manager.show('listings');
+        })
+        .catch((err) => {
+          console.error(err.message);
+          views_manager.show('listings');         
+        });
+      });
+    }
+  });
 
   window.$newReviewForm = $newReviewForm;
 });
+
+
