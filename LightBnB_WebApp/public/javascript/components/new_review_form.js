638c2a5aa98dffd41e847b267db131328e006b13
Lendl Peralta <janlendl@gmail.com>
2021-10-19 01:29:06 +0000

    changed double quotes to single quotes

--- LightBnB_WebApp/public/javascript/components/new_review_form.js
+++ LightBnB_WebApp/public/javascript/components/new_review_form.js
@@ -1,18 +1,18 @@
 $(() => {
   const $newReviewForm = $(`
-    <form>
-      <textarea id="new-review-body" rows="4 cols="50"></textarea>
-      <label for="new-review-rating">Select a rating:</label>
-      <select name="rating" id="new-review-rating">
-        <option value="">-- Select a rating --</option>
-        <option value="1">1 star</option>
-        <option value="2">2 stars</option>
-        <option value="3">3 stars</option>
-        <option value="4">4 stars</option>
-        <option value="5">5 stars</option>
+    <form class='new-review-form'>
+      <textarea id='new-review-body' rows='4' cols='50'></textarea>
+      <label for='new-review-rating'>Select a rating:</label>
+      <select name='rating' id='new-review-rating'>
+        <option value=''>-- Select a rating --</option>
+        <option value='1'>1 star</option>
+        <option value='2'>2 stars</option>
+        <option value='3'>3 stars</option>
+        <option value='4'>4 stars</option>
+        <option value='5'>5 stars</option>
       </select>
-      <div id="datatag" class="hidden"></div>
-      <button type="submit">Submit</button>
+      <div id='datatag' class='hidden'></div>
+      <button type='submit'>Submit</button>
     </form>
   `);
 
@@ -22,8 +22,8 @@ $(() => {
     const reviewRating = $('#new-review-rating').val();
     const reservationId = $('#datatag h4').text();
     // clear our review fields
-    $('#new-review-rating').val("");
-    $("#new-review-body").val("");
+    $('#new-review-rating').val('');
+    $('#new-review-body').val('');
     if (reviewRating && reservationId) {
       getIndividualReservation(reservationId)
       .then(data => {
